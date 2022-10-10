f1b421ff4489a33512b0329650f27934a432a9db
omognuni <melbong0069@gmail.com>
2022-10-10 13:55:23 +0900

    [Feat] Company 모델 추가

--- wanted/core/models.py
+++ wanted/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
