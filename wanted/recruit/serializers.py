c47535b5f5fb9f51a5ba713bb2cdfa19601f34df
yongun414 <melbong0069@gmail.com>
2022-10-11 14:59:32 +0900

    [Feat] 채용 공고 상세 페이지 구현

--- wanted/recruit/serializers.py
+++ wanted/recruit/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from django.db.models import Q
+
 from core.models import Recruit
 
 
@@ -7,10 +9,21 @@ class RecruitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recruit
-        fields = ['id', 'title','position', 'company', 'reward', 'stack']
+        fields = ['id', 'title', 'position', 'company', 'reward', 'stack']
         read_only_fields = ['id']
 
+
 class RecruitDetailSerializer(RecruitSerializer):
+    related_ad = serializers.SerializerMethodField(
+        method_name='get_related_ad')
+
+    def get_related_ad(self, obj):
+        recruits = Recruit.objects.filter(
+            company=obj.company.id).filter(~Q(id=obj.id)).values('id')
+
+        data = [recruit['id'] for recruit in recruits]
+
+        return data
 
     class Meta(RecruitSerializer.Meta):
-        fields = RecruitSerializer.Meta.fields + ['description']
+        fields = RecruitSerializer.Meta.fields + ['description', 'related_ad']
