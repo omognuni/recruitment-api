5e0c6f50379ab344a005eef60f82e136d0d472ee
yongun414 <melbong0069@gmail.com>
2022-10-11 17:06:49 +0900

    [Feat] Recruit 모델에 user id추가

--- wanted/core/models.py
+++ wanted/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class Company(models.Model):
@@ -16,8 +17,11 @@ class Recruit(models.Model):
     position = models.CharField(max_length=255)
     reward = models.IntegerField()
     description = models.TextField(blank=True)
-    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    company = models.ForeignKey(
+        'Company', on_delete=models.SET_NULL, blank=True, null=True)
     stack = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
