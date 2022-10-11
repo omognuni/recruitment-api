c3548eaa0d19395a2b05ee4654cc19f32cca8233
yongun414 <melbong0069@gmail.com>
2022-10-11 17:47:49 +0900

    [Feat] Recruit 페이지 user authentication 구현

--- wanted/recruit/views.py
+++ wanted/recruit/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import BasicAuthentication
 from core.models import Recruit
 from recruit import serializers
 
@@ -7,7 +8,12 @@ from recruit import serializers
 class RecruitViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecruitDetailSerializer
     queryset = Recruit.objects.all()
-    
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action == 'list':
             return serializers.RecruitSerializer
