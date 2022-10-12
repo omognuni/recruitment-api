82429ffaf53c0ba4bc035db5080e67461f77b9ee
yongun414 <melbong0069@gmail.com>
2022-10-12 15:24:30 +0900

    [Test] User 모델 테스트

--- wanted/user/tests/test_user_api.py
+++ wanted/user/tests/test_user_api.py
@@ -4,3 +4,30 @@ from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserAPITests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user(self):
+        payload = {
+            'username': 'testuser',
+            'password': 'testpass',
+            'name': 'testname'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(username=payload['username'])
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
