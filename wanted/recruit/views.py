3b8b74ffee2e3cb6a6dcab6b31633d3b5dfde1ca
yongun414 <melbong0069@gmail.com>
2022-10-12 10:35:37 +0900

    [Refactor] 중복 속성 정리

--- wanted/recruit/views.py
+++ wanted/recruit/views.py
@@ -1,16 +1,18 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.authentication import BasicAuthentication
-from core.models import Recruit, Company
+from core.models import Recruit, Company, Apply
 from recruit import serializers
 
 
-class RecruitViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.RecruitDetailSerializer
-    queryset = Recruit.objects.all()
+class BaseRecruitAttrViewSet(viewsets.ModelViewSet):
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
+
+class RecruitViewSet(BaseRecruitAttrViewSet):
+    serializer_class = serializers.RecruitDetailSerializer
+    queryset = Recruit.objects.all()
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -18,8 +20,6 @@ class RecruitViewSet(viewsets.ModelViewSet):
         return self.serializer_class
 
 
-class CompanyViewSet(viewsets.ModelViewSet):
+class CompanyViewSet(BaseRecruitAttrViewSet):
     serializer_class = serializers.CompanySerializer
     queryset = Company.objects.all()
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
