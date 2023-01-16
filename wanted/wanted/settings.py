4f8f5d036b205740d8531f980b82f88e264c8e84
omognuni <melbong0069@gmail.com>
2023-01-16 22:32:36 +0900

    [Chore] query log 설정

--- wanted/wanted/settings.py
+++ wanted/wanted/settings.py
@@ -131,3 +131,20 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
